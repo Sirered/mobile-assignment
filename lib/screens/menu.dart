3c23f3e88a52605bd4f7941b78284d9f31dc8113
Sirered <gikurniawan.1995@gmail.com>
2023-11-13 20:30:02 +0700

    doing the tutorial stuff

---
+++ lib/screens/menu.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_inventory/widgets/left_drawer.dart';
+import 'package:mobile_inventory/widgets/shop_card.dart';
+
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  final List<ShopItem> items = [
+    ShopItem("View Items", Icons.checklist, Colors.indigo),
+    ShopItem("Add Item", Icons.add_shopping_cart, Colors.orange),
+    ShopItem("Logout", Icons.logout, Colors.red),
+    ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Inventory',
+        ),
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+      ),
+      drawer: const LeftDrawer(),
+      body: SingleChildScrollView(
+        // Scrolling wrapper widget
+        child: Padding(
+          padding: const EdgeInsets.all(10.0), // Set padding for the page
+          child: Column(
+            // Widget to display children vertically
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                // Text widget to display text with center alignment and appropriate style
+                child: Text(
+                  'PBP Shop', // Text indicating the shop name
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              // Grid layout
+              GridView.count(
+                // Container for our cards.
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                children: items.map((ShopItem item) {
+                  // Iteration for each item
+                  return ShopCard(item);
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/widgets/shop_card.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_inventory/screens/inventory_form.dart';
+
+class ShopItem {
+  final String name;
+  final IconData icon;
+  final Color color;
+
+  ShopItem(this.name, this.icon, this.color);
+}
+
+class ShopCard extends StatelessWidget {
+  final ShopItem item;
+
+  const ShopCard(this.item, {Key? key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: item.color,
+      child: InkWell(
+        // Responsive touch area
+        onTap: () {
+          // Show a SnackBar when clicked
+          ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(SnackBar(
+            content: Text("You pressed the ${item.name} button!")
+          ));
+          if (item.name == "Add Item") {
+            Navigator.push(
+              context, 
+              MaterialPageRoute(builder:(context) => const ShopFormPage())
+            );
+          }
+        },
+        child: Container(
+          // Container to hold Icon and Text
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
